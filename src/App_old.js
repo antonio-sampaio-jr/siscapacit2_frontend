90a1f2377c5d1795def83e66473a53300f6d434f
Antonio Sampaio Jr <assoftbel@gmail.com>
2022-12-14 11:20:01 -0300

    OK

--- src/App_old.js
+++ src/App_old.js
@@ -14,17 +14,20 @@ import CourseList from "./components/Courses/CourseList/CourseList";
 import CourseDetails from "./components/Courses/CourseDetails/CourseDetails";
 import AddCourse from "./components/Courses/AddCourse/AddCourse";
 import EditCourse from "./components/Courses/EditCourse/EditCourse";
-import ServPage from "./components/User/ServidorMode/ServPage";
-import SaibamaisCourse from "./components/User/ServidorMode/SaibamaisCourse";
-import pageInscricao from "./components/User/ServidorMode/pageInscricao";
-import MeusCursos from "./components/User/ServidorMode/CadastroServidor/MeusCursos";
+import Login from "./components/Autentication/Login";
+import Register from "./components/Autentication/Register";
+import GovEmployeeCoursesList from "./components/Users/GovEmployeeCoursesList";
+import GovEmployeeCourseDetails from "./components/Users/GovEmployeeCourseDetails";
 
 function App() {
+  
   const apiURLCourses = "https://siscapacit2-api.cyclic.app/cursos";
   const apiURL = "https://siscapacit2-api.cyclic.app/servidores";
+  const apiURLAdmin = "https://siscapacit2-api.cyclic.app/administradores";
+  //comentário
 
   const [form, setForm] = useState({
-    matricula: "",
+    matricula: "", 
     nome: "",
     foto: "",
     orgao: "",
@@ -63,7 +66,12 @@ function App() {
     <div className="App bg-light" style={{ height: "100vh" }}>
       <NavigationBar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Login apiURL={apiURL} apiURLAdmin={apiURLAdmin}/>} />
+        <Route path="/register" element={<Register apiURL={apiURL}/>} />
+
+        <Route path="/listarCursosAluno" element={<GovEmployeeCoursesList apiURL={apiURLCourses}/>} />
+        <Route path="/listarCursoAluno/:idCurso/6398e1849a7cc8fb4d7f1d33" element={<GovEmployeeCourseDetails apiURL={apiURLCourses}/>} />
+        
         <Route
           path="/listarServidores"
           element={<GovEmployeeList apiURL={apiURL} />}
@@ -112,11 +120,6 @@ function App() {
             />
           }
         />
-        <Route path="/pageServidor" element={<ServPage />} />
-        <Route path="/saibaMaisCourse/:id" element={<SaibamaisCourse />} />
-        <Route path="/meusCursos" element={<MeusCursos />} />
-        <Route path="/pageInscricao" element={<pageInscricao />} />
-
         <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
